2b6f291c7d38263e46e359527d5abf2dac1a8e20
Miguel Costa <miguel.costa@inelcis.pt>
2017-06-02 17:18:37 +0100

    Add tsconfig.json

--- src/multimath/app/app.ts
+++ src/multimath/app/app.ts
@@ -2,6 +2,7 @@ function startGame() {
   // starting a new game
   var messagesElement = document.getElementById('messages');
   messagesElement.innerHTML= 'Welcome to MultiMath! Starting new game...';
+  console.log("Starting new game");
 }
 
 document.getElementById('startGame').addEventListener('click', startGame);
--- src/multimath/js/app.js
+++ src/multimath/js/app.js
@@ -2,5 +2,7 @@ function startGame() {
     // starting a new game
     var messagesElement = document.getElementById('messages');
     messagesElement.innerHTML = 'Welcome to MultiMath! Starting new game...';
+    console.log("Starting new game");
 }
 document.getElementById('startGame').addEventListener('click', startGame);
+//# sourceMappingURL=app.js.map
