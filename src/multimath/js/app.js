d4636ed354953efbdd6670bc23246503e17173d3
Miguel Costa <miguel.costa@inelcis.pt>
2017-06-02 17:23:59 +0100

    Use tsconfig.base

--- src/multimath/js/app.js
+++ src/multimath/js/app.js
@@ -1,5 +1,4 @@
 function startGame() {
-    // starting a new game
     var messagesElement = document.getElementById('messages');
     messagesElement.innerHTML = 'Welcome to MultiMath! Starting new game...';
     console.log("Starting new game");
